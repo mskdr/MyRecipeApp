92d80e637af208862e61d57f2deb1bd5786c0aec
Muhammet KDR <mskdr1453@gmail.com>
2022-11-23 19:10:03 +0300

    base fragment-extensions-resource class

--- app/src/main/java/com/muhammetkdr/myrecipeapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/muhammetkdr/myrecipeapp/ui/home/HomeFragment.kt
@@ -1,7 +1,23 @@
 package com.muhammetkdr.myrecipeapp.ui.home
 
-import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.muhammetkdr.myrecipeapp.base.BaseFragment
+import com.muhammetkdr.myrecipeapp.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class HomeFragment : Fragment() {
+@AndroidEntryPoint
+class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>(
+    FragmentHomeBinding::inflate
+) {
+    override val viewModel by viewModels<HomeViewModel>()
+
+
+    override fun onCreateFinished() {
+
+    }
+
+    override fun observeEvents() {
+    
+    }
 
 }
