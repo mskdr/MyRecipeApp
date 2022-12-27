19404c0ed4b80fc86870c892d91eca348663e378
Muhammet KDR <mskdr1453@gmail.com>
2022-12-27 15:42:52 +0300

    adapter adjustment

--- app/src/main/java/com/muhammetkdr/myrecipeapp/common/utils/BindingAdapters.kt
+++ app/src/main/java/com/muhammetkdr/myrecipeapp/common/utils/BindingAdapters.kt
@@ -2,22 +2,22 @@ package com.muhammetkdr.myrecipeapp.common.utils
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import androidx.lifecycle.LiveData
 import com.bumptech.glide.Glide
 import com.muhammetkdr.myrecipeapp.R
 
 @BindingAdapter("setImage")
-fun setImage(imageView: ImageView, imageUrl: String?) {
-    Glide.with(imageView).load(imageUrl).into(imageView)
+fun ImageView.setImage(imageUrl: String?) {
+    Glide.with(this).load(imageUrl).into(this)
 }
 
 @BindingAdapter("setIngredients")
-fun setIngredients(imageView: ImageView, ingredient: String?) {
-    Glide.with(imageView).load("https://www.themealdb.com/images/ingredients/${ingredient}-Small.png").into(imageView)
+fun ImageView.setIngredients(ingredient: String?) {
+    Glide.with(this).load("https://www.themealdb.com/images/ingredients/${ingredient}-Small.png").into(this)
 }
 
 @BindingAdapter("favoriteState")
-fun favoriteState(imageView: ImageView, isFavorite: Boolean) {
-    if (isFavorite) imageView.setImageResource(R.drawable.ic_favorite_selected)
-    else imageView.setImageResource(R.drawable.ic_favorite_unselected)
+fun ImageView.favoriteState(isFavorite: LiveData<Boolean>) {
+    if (isFavorite.value == true) this.setImageResource(R.drawable.ic_favorite_selected)
+    else this.setImageResource(R.drawable.ic_favorite_unselected)
 }
-
