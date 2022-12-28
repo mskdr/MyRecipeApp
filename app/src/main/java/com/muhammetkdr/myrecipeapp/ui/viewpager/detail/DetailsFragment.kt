84e4942ea835f2eacd0da91354ddd7c52d1666e9
Muhammet KDR <mskdr1453@gmail.com>
2022-12-28 12:00:52 +0300

    In order to delete favorite item, added alert dialog

--- app/src/main/java/com/muhammetkdr/myrecipeapp/ui/viewpager/detail/DetailsFragment.kt
+++ app/src/main/java/com/muhammetkdr/myrecipeapp/ui/viewpager/detail/DetailsFragment.kt
@@ -2,7 +2,9 @@ package com.muhammetkdr.myrecipeapp.ui.viewpager.detail
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.muhammetkdr.myrecipeapp.R
 import com.muhammetkdr.myrecipeapp.base.BaseFragment
 import com.muhammetkdr.myrecipeapp.common.extensions.gone
@@ -25,7 +27,7 @@ class DetailsFragment : BaseFragment<FragmentDetailsBinding, DetailsViewModel>(
         initDataBinding()
     }
 
-    fun initDataBinding() = with(binding){
+    fun initDataBinding() = with(binding) {
         detailsViewModel = viewModel
         lifecycleOwner = viewLifecycleOwner
     }
@@ -35,8 +37,10 @@ class DetailsFragment : BaseFragment<FragmentDetailsBinding, DetailsViewModel>(
             with(binding) {
                 when (Resource) {
                     is Resource.Success -> {
-                        Resource.data.let { it ->
-                            meal = it.meals!![0] // There is only 1 meal will come in here as a response
+                        Resource.data.let { mealModel ->
+                            mealModel.meals?.let {
+                                meal = mealModel.meals[0]  // There is only 1 meal will come in here as a response
+                            }
                             detailViewGroup.visible()
                             detailsProgressBar.gone()
                         }
@@ -50,6 +54,38 @@ class DetailsFragment : BaseFragment<FragmentDetailsBinding, DetailsViewModel>(
                         detailViewGroup.invisible()
                         detailsProgressBar.visible()
                     }
+                }
+            }
+        }
+
+        viewModel.isFavorite.observe(viewLifecycleOwner) { favorite ->
+            initImgFavOnClick(favorite)
+        }
+    }
+
+    fun initImgFavOnClick(isFavorite: Boolean) = with(binding) {
+        imgFavorite.setOnClickListener {
+            if (isFavorite) {
+                MaterialAlertDialogBuilder(requireContext())
+                    .setTitle(resources.getString(R.string.dialog_title))
+                    .setMessage(resources.getString(R.string.dialog_body))
+                    .setPositiveButton(resources.getString(R.string.dialog_accept)) { dialog, which ->
+                        Toast.makeText(
+                            requireContext(), R.string.toast_accepted, Toast.LENGTH_SHORT
+                        ).show()
+
+                        meal?.let { meal ->
+                            viewModel.setFavoriteState(meal)
+                        }
+                    }
+                    .setNegativeButton(resources.getString(R.string.dialog_decline)) { dialog, which ->
+                        Toast.makeText(requireContext(), R.string.toast_declined, Toast.LENGTH_SHORT
+                        ).show()
+                    }.show()
+            } else {
+                Toast.makeText(requireContext(), R.string.toast_added, Toast.LENGTH_SHORT).show()
+                meal?.let { meal ->
+                    viewModel.setFavoriteState(meal)
                 }
             }
         }
