3ac6c90bae461a24545f2456e5257933b7db7626
Muhammet KDR <mskdr1453@gmail.com>
2023-01-16 21:48:08 +0300

    base url moved to build config

--- app/src/main/java/com/muhammetkdr/myrecipeapp/common/utils/Const.kt
+++ app/src/main/java/com/muhammetkdr/myrecipeapp/common/utils/Const.kt
@@ -2,7 +2,6 @@ package com.muhammetkdr.myrecipeapp.common.utils
 
 class Const {
     companion object{
-        const val BASE_URL = "https://www.themealdb.com/"
         const val DETAIL_PAGE_INDEX = 0
         const val INGREDIENTS_PAGE_INDEX = 1
     }
